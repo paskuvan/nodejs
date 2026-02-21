c22b87f0a603615b73f796a8e72e796a7ded3a67
Majo Paskuvan <majopaskuvan@gmail.com>
2026-02-21 17:13:09 -0300

    add auth logout

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+// routes import
+import UserRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", UserRouter);
+
+// example route: http://localhost:4000/api/v1/users/register
 
 
 export default app;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,2 +1,56 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            minLength: 1,
+            maxLength: 30,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            minLength: 6,
+            maxLength: 50,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        }
+
+    },
+
+        {
+            timestamps: true,
+        }
+                
+)
+
+// before saving the user, hash the password
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this,password = await bcrypt.hash(this.password, 10);
+
+    next();
+
+});
+
+// compare the password
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+export const User = mongoose.model("User", userSchema)
